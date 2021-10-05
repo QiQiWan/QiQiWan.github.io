cd57cf1c87aba6ca923d07d8b70c344be542f83a
eatrice <1521371275@qq.com>
2021-10-05 22:23:52 +0800

    Site updated: 2021-10-05 06:23:30

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/img/favcion.png","e02488f887ee978e139945973541f1ac"],["/index.html","522ac2c5a2117bae159cd1a01a01cdd7"],["/manifest.json","4ea3c9cebfe8a7325ee37a1c98d517a6"]];
+var precacheConfig = [["/img/favcion.png","e02488f887ee978e139945973541f1ac"],["/index.html","0797cdab7221780ae1670385acb4c0cc"],["/manifest.json","4ea3c9cebfe8a7325ee37a1c98d517a6"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
